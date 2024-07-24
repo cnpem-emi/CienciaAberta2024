441dbdbce29d21094874d093a933dacbf54af1d0
Rodrigo da Silva Moraes <rodrigo.moraes@cnpem.br>
2024-07-24 09:31:17 -0300

    Aded serial config on main

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,6 @@ OpticalSensor sensor3(SENSOR_3_PIN, SENSOR_3_OUTPUT, 20, AUTOMATIC_MODE);
 
 //ButtonControl btn(50);
 
-int last_sensor = 0;
-
 void setup () {
   Serial.begin(9600);
   sensor1.config();
@@ -28,24 +26,28 @@ void setup () {
 }
 
 void loop() {
-  //btn.readButtonMatrix();
-  sensor1.loop();
-  sensor2.loop();
-  sensor3.loop();
-  if (sensor1.measure_speed == true || sensor2.measure_speed == true || sensor3.measure_speed == true){
-    serial.getInfo(velocity.get_speed());
-    json = serial.sendInfo();
-    Serial.println(json);
-    //Serial.println(velocity.last_time);
-    sensor1.measure_speed = false;
-    sensor2.measure_speed = false;
-    sensor3.measure_speed = false;
+  if (serial.send_data == false) {
+    serial.config();
+  } else {
+    //btn.readButtonMatrix();
+    sensor1.loop();
+    sensor2.loop();
+    sensor3.loop();
+    if (sensor1.measure_speed == true || sensor2.measure_speed == true || sensor3.measure_speed == true){
+      serial.getInfo(velocity.get_speed());
+      json = serial.sendInfo();
+      Serial.println(json);
+
+      sensor1.measure_speed = false;
+      sensor2.measure_speed = false;
+      sensor3.measure_speed = false;
+    }
+    
+    GPIO.out_w1tc = (1 << SENSOR_1_OUTPUT);
+    GPIO.out_w1tc = (1 << SENSOR_2_OUTPUT);
+    GPIO.out_w1tc = (1 << SENSOR_3_OUTPUT);
+    GPIO.out_w1tc = (1 << SENSOR_4_OUTPUT);
+    GPIO.out_w1tc = (1 << SENSOR_5_OUTPUT);
+    GPIO.out_w1tc = (1 << SENSOR_6_OUTPUT);
   }
-  
-  GPIO.out_w1tc = (1 << SENSOR_1_OUTPUT);
-  GPIO.out_w1tc = (1 << SENSOR_2_OUTPUT);
-  GPIO.out_w1tc = (1 << SENSOR_3_OUTPUT);
-  GPIO.out_w1tc = (1 << SENSOR_4_OUTPUT);
-  GPIO.out_w1tc = (1 << SENSOR_5_OUTPUT);
-  GPIO.out_w1tc = (1 << SENSOR_6_OUTPUT);
 }
