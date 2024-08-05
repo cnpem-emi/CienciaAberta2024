9ca38178287c42545be339a55171854b8b6a34ca
Rodrigo da Silva Moraes <rodrigo.moraes@cnpem.br>
2024-08-05 11:54:03 -0300

    Added a delay at the serial config

--- lib/com_port/com_port.cpp
+++ lib/com_port/com_port.cpp
@@ -2,6 +2,7 @@
 
 void ComPort::config() {
     Serial.println("ESP 32 trying to connect witbhh the GUI...");
+    delay(200);
     if (Serial.available() > 0){
         this->incoming_data = Serial.readString();
         Serial.write(0x01);
@@ -13,5 +14,6 @@ void ComPort::getInfo(float speed) {
 }
 
 String ComPort::sendInfo() {
+    delay(1);
     return doc.as<String>();
 }
