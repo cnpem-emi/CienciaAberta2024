3ec45015a4678763018acb1daad1728aa37eab24
Rodrigo da Silva Moraes <rodrigo.moraes@cnpem.br>
2024-08-07 10:47:27 -0300

    receiving data

--- ca2024/esp_serial.py
+++ ca2024/esp_serial.py
@@ -52,8 +52,9 @@ class ComPort:
         """
 
         while 1:
-            message = self.device.readline().decode('utf-8').rstrip()
+            message = self.device.readline().decode('utf-8')
             print(message)
+            print(message[1:-3])
             
             if message[0] == "#" and message[-1] == "$":
                 return float(message)
